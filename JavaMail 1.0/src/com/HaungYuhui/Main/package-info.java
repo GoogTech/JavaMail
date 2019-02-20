a3b770601404ee26ed093c7481b8b104fb1b4b22
HuangYuhui <Gentleman_0109@outlook.com>
2019-02-20 20:00:15 +0800

    Mail 1.0 : Success to send the email to the specified email adress.

---
+++ JavaMail 1.0/src/com/HaungYuhui/Main/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @Project JavaMail 1.0
+ * @Package com.HaungYuhui.Main
+ * @Description TODO
+ * @Author HuangYuhui
+ * @Date Feb 20, 2019-1:32:21 PM
+ * @version 1.0
+ */
+package com.HaungYuhui.Main;
